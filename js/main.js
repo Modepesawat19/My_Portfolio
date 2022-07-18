1cccb4d30baab9a52bddb896034024cff6089755
Modepesawat19 <sofyanhariri19@gmail.com>
2022-07-19 00:52:51 +0700

    first commit

---
+++ js/main.js
@@ -0,0 +1,42 @@
+// var x = 5;
+// console.log('Hello World');
+// console.log('isi dari variable x adalah '+ x);
+
+
+
+
+// pop up box//
+// snyntax//
+// alert('halo!');
+// alert('nama');
+// alert('Sofyan');
+
+// var nama = prompt('masukkan nama:');
+// alert (nama)
+
+// confirm('Kamu Yakin??');
+
+// pengkondisian//
+/*  
+    var tes = confirm('Kamu Yakin??');
+if (tes === true) {
+    alert('user menekan OK');
+}
+else {
+    alert('user menekan CANCEL')
+}
+*/
+
+
+// pengulangan//
+// alert('selamat datang..');
+// var lagi = true;
+// while( lagi === true ) {
+    // var nama = prompt ('masukkan nama:');
+    // alert('halo ' + nama);
+
+    // lagi = confirm('coba lagi?');
+
+// }
+
+// alert ('terima kasih..');
